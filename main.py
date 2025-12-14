802c07df7b2f834ca1386edbb01b94fc8adc9c4d
AdrianBernabeuNotari <adrian.bernabeu@hotmail.com>
2025-12-14 11:45:05 +0100

    comentado simple
    Comentado simple y comprensivo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.graph import app_graph
+from app.graph import app_graph # esto importa la cadena de agentes ya creada
 
 app = FastAPI(
     title="Nombre de la app",
@@ -7,13 +7,20 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# endpoint para el chatbot
 @app.post("/api/chat")
 async def chat(mensaje: str):
-    inputs = {"mensaje": mensaje}
-    resultado = app_graph.invoke(inputs)
-    return {"respuesta": resultado['final_response']}
+    inputs = {"mensaje": mensaje} # 1
+    resultado = app_graph.invoke(inputs) # 2
+    return {"respuesta": resultado['final_response']} # 3
+
+    # lo que tiramos aquí:
+    # 1: metemos el mensaje del usuario
+    # 2: generamos la respuesta
+    # 3: la devolvemos
 
 
+# este endpoint sirve para verificar de que el servidor funcione
 @app.get("/api/health")
 def health_check():
     return {"status": "online", "system": "Versión 1.0.0"}
